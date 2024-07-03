1fa30e91274cab6f2a320639ced69b6750956acc
Marcus Holanda <mvsh777@hotmail.com>
2024-07-03 15:43:58 -0300

    primeira distro

--- athena_mvsh/cursores/__init__.py
+++ athena_mvsh/cursores/__init__.py
@@ -1,7 +1,7 @@
-from cursores.cursorparquet import CursorParquet
-from cursores.cursorparquetduckdb import CursorParquetDuckdb
-from cursores.cursorpython import CursorPython
-from cursores.cursores import CursorBaseParquet, CursorIterator
+from athena_mvsh.cursores.cursorparquet import CursorParquet
+from athena_mvsh.cursores.cursorparquetduckdb import CursorParquetDuckdb
+from athena_mvsh.cursores.cursorpython import CursorPython
+from athena_mvsh.cursores.cursores import CursorBaseParquet, CursorIterator
 
 __all__ = [
     'CursorParquet',

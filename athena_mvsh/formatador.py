5ec8bc8ebd61e81f5f1354358dc0cf40f84eb496
Marcus Holanda <mvsh777@hotmail.com>
2025-02-28 09:13:17 -0300

    feat(:main:) formatador de sequencia corrigida

--- athena_mvsh/formatador.py
+++ athena_mvsh/formatador.py
@@ -34,26 +34,43 @@ def get_value_format(v):
     if isinstance(v, str):
         return escape_presto(v)
 
-    # @experimental
-    if isinstance(v, (list, set, tuple)):
-        return ','.join(f'{val!r}' for val in v)
-
     if isinstance(v, Decimal):
         value = escape_presto(f'{v:f}')
-        return f"DECIMAL {value}"
+        return f'DECIMAL {value}'
 
     if isinstance(v, (int, float)):
         return v
+
+
+def cast_sequence_format(arg) -> str:
+    for i, val in enumerate(arg):
+        value_format = get_value_format(val)
+
+        if not isinstance(value_format, (str,)):
+            if isinstance(value_format, float):
+                arg[i] = f'{value_format:f}'
+            else:
+                arg[i] = f'{value_format}'
+        else:
+            arg[i] = value_format
+
+    return ','.join(arg)
 
 
 def cast_format(consulta: str, *args, **kwargs) -> str:
     if args:
         args = list(args)
         for p, arg in enumerate(args):
-            args[p] = get_value_format(arg)
+            if isinstance(arg, (list, set, tuple)):
+                args[p] = cast_sequence_format(arg)
+            else:
+                args[p] = get_value_format(arg)
 
     if kwargs:
         for k, v in kwargs.items():
-            kwargs[k] = get_value_format(v)
+            if isinstance(v, (list, set, tuple)):
+                kwargs[k] = cast_sequence_format(v)
+            else:
+                kwargs[k] = get_value_format(v)
 
     return consulta.format(*args, **kwargs)
--- tests/test_format.py
+++ tests/test_format.py
@@ -91,3 +91,44 @@ def test_select():
     )
 
     assert rst == esperado
+
+
+def test_list():
+    esperado = textwrap.dedent(
+        """
+        SELECT *
+        FROM "teste"."teste"
+        WHERE num in(10,20)
+        and periodo in(TIMESTAMP '2025-01-01 00:00:00.000000',TIMESTAMP '2025-12-31 23:59:59.999999')
+        and nome in(null,null)
+        and valor in(DECIMAL '1.000',DECIMAL '2.000')
+        and sobre in('tp','sp')
+        and floats in(1.100000,2.200000)
+        and date in(DATE '2021-01-01',DATE '2022-01-01')
+        """
+    ).strip()
+
+    rst = cast_format(
+        textwrap.dedent(
+            """
+            SELECT *
+            FROM "teste"."teste"
+            WHERE num in({})
+            and periodo in({})
+            and nome in({})
+            and valor in({})
+            and sobre in({})
+            and floats in({})
+            and date in({})
+            """
+        ).strip(),
+        [10, 20],
+        [datetime(2025, 1, 1), datetime(2025, 12, 31, 23, 59, 59, 999999)],
+        [None, None],
+        [Decimal('1.000'), Decimal('2.000')],
+        ['tp', 'sp'],
+        [1.1, 2.2],
+        [date(2021,1,1), date(2022,1,1)]
+    )
+
+    assert rst == esperado

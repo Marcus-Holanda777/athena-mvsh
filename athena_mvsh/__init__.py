5622d9f54fd7b7163c9c7b173e6fe92f3e2c0528
Marcus Holanda <mvsh777@hotmail.com>
2025-04-28 11:21:52 -0300

    feat(:main:) add author modulo

--- athena_mvsh/__init__.py
+++ athena_mvsh/__init__.py
@@ -2,5 +2,6 @@ from athena_mvsh.connection import Athena
 from athena_mvsh.cursores import CursorParquet, CursorParquetDuckdb, CursorPython
 
 __version__ = '0.0.19'
+__author__ = 'Marcus Holanda'
 
 __all__ = ['Athena', 'CursorParquetDuckdb', 'CursorPython', 'CursorParquet']

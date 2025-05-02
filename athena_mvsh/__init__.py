6c9f760838dfb1896ee61c4d89c4ab634855a7a3
Marcus Holanda <mvsh777@hotmail.com>
2025-05-02 17:33:23 -0300

    feat(:main:) bug versoes do python

--- athena_mvsh/__init__.py
+++ athena_mvsh/__init__.py
@@ -1,7 +1,7 @@
 from athena_mvsh.connection import Athena
 from athena_mvsh.cursores import CursorParquet, CursorParquetDuckdb, CursorPython
 
-__version__ = '0.0.20'
+__version__ = '0.0.21'
 __author__ = 'Marcus Holanda'
 
 __all__ = ['Athena', 'CursorParquetDuckdb', 'CursorPython', 'CursorParquet']
